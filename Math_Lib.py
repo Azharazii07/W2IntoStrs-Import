2a0e0372a42a0e18f9252e7f6b057eb855ecb86c
Shaik Muhammad Azharuddin <25etim07@uohyd.ac.in>
2026-07-19 00:39:24 +0530

    Create Math_Lib.py

---
+++ Math_Lib.py
@@ -0,0 +1,9 @@
+# Importing Packages --- 1. math library
+import math
+print(math.log(1))
+print(math.sin(0))
+print(math.cos(0))
+print(math.sqrt(16))
+print(math.cbrt(729))
+print(math.factorial(4))
+print(math.pow(2,10))
