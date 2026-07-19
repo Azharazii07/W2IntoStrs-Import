1eb56bf848a9b62c83b693830918960e417bbac9
Shaik Muhammad Azharuddin <25etim07@uohyd.ac.in>
2026-07-19 09:22:01 +0530

    Create F_strings.py

---
+++ F_strings.py
@@ -0,0 +1,11 @@
+age = 12 
+mon = 4
+n = 3.567
+print("Your age is",age,"and mon is",mon)
+print("Your age is "+ str(age) +" and mon is "+ str(mon) )
+print(F"Your age is {age} and mon is {mon}")
+print(f"{age = } and {mon = }")
+print(f"{n = }")
+print(f"{0.5:0.03f}")
+print(f"{n :0.2f}")
+print(f"{n :04.2f}")
